3519d1ba71ad0603f1df51184d9230e505814409
Ali Dakoumi <dakoumi31@gmail.com>
2022-11-29 13:48:25 +0100

    images optimization

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="ar">
       <Head>
-        <meta name="title" content="Tattica - تاتيكا " />
+        {/* <meta name="title" content="Tattica - تاتيكا " />
         <meta
           name="description"
           content="موقع رياضي مختص في التكتيك و تصحيح المفاهيم - هنا تجد نخبة المحللين العرب"
@@ -38,7 +38,7 @@ export default function Document() {
         <meta
           property="twitter:image"
           content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
-        ></meta>
+        ></meta> */}
       </Head>
       <body>
         <Main />
