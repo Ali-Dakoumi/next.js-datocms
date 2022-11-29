958e28d87494c2d6dca09c6f5c48e05632124d36
Ali Dakoumi <dakoumi31@gmail.com>
2022-11-29 14:12:30 +0100

    images optimization

--- components/indexHead.jsx
+++ components/indexHead.jsx
@@ -4,35 +4,30 @@ import React from 'react'
 export default function IndexHead() {
   return (
     <Head>
-      <meta name="title" content="Tattica - تاتيكا " />
+      <title>Tattica - تاتيكا</title>
+      <meta name="title" content="Tattica - تاتيكا" />
       <meta
         name="description"
         content="موقع رياضي مختص في التكتيك و تصحيح المفاهيم - هنا تجد نخبة المحللين العرب"
       />
-      <meta
-        name="keywords"
-        content="football, tattica, kora, كورة , tactic, coach, match analysis, تحليل , تكتيك , مباراة , كرة قدم "
-      />
-      <meta name="robots" content="index, follow" />
-      <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-      <meta name="language" content="Arabic" />
+
       <meta property="og:type" content="website" />
-      <meta property="og:url" content="https://tattica.io/ " />
+      <meta property="og:url" content="https://next-js-datocms.vercel.app/" />
       <meta property="og:title" content="Tattica - تاتيكا" />
       <meta
         property="og:description"
-        content="موقع رياضي مختص في التكتيك و تصحيح المفاهيم - هنا تجد نخبة المحللين العرب "
+        content="موقع رياضي مختص في التكتيك و تصحيح المفاهيم - هنا تجد نخبة المحللين العرب"
       />
       <meta property="og:image" content="/image.svg" />
 
-      <meta property="twitter:card" content="/image.svg" />
-      <meta property="twitter:url" content="https://tattica.io/" />
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content="https://next-js-datocms.vercel.app/" />
       <meta property="twitter:title" content="Tattica - تاتيكا" />
       <meta
         property="twitter:description"
-        content="موقع رياضي مختص في التكتيك و تصحيح المفاهيم - هنا تجد نخبة المحللين العرب "
+        content="موقع رياضي مختص في التكتيك و تصحيح المفاهيم - هنا تجد نخبة المحللين العرب"
       />
-      <meta property="twitter:image" content="/image.svg"></meta>
+      <meta property="twitter:image" content="/image.svg" />
       <title>Tattica</title>
       <link rel="icon" href="/favicon.ico" />
     </Head>
