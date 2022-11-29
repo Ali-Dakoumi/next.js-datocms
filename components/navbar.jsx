405684cc5023196969ae04137ad03e3237b45598
Ali Dakoumi <dakoumi31@gmail.com>
2022-11-29 11:47:22 +0100

    seo

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -8,7 +8,7 @@ export default function Navbar() {
     <ul className="maw-w-full flex justify-between items-center w-full text-textwhite py-2">
       <li className="mt-2 relative w-12 h-7 md:w-20 md:h-12 font-ilyas ml-2 text-[2rem] md:text-[3rem]">
         <Link href={'/'}>
-          <Image src={'/white-logo.svg'} fill />
+          <Image src={'/white-logo.svg'} fill alt="logo" />
         </Link>
       </li>
       <li className="mr-2">
