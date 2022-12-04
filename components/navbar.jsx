6ab8d2fbd8f254b364e161aa0a9ca95df6870aec
Ali Dakoumi <dakoumi31@gmail.com>
2022-12-04 15:06:16 +0100

    paragraph styles and buttons

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -7,7 +7,7 @@ export default function Navbar() {
   // const setSearchVariable = useSetSearchVariable()
   // const setSearchBool = useSetSearchBool()
   return (
-    <ul className="maw-w-full flex justify-between items-center w-full text-textwhite py-1">
+    <ul className="maw-w-full flex justify-between items-center w-full text-textwhite pt-2 pb-1">
       <li className=" relative w-12 h-7 font-ilyas ml-2 text-[2rem] md:text-[3rem]">
         <Link
           href={'/'}
